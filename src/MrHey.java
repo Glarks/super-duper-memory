520141ecc38528e85a8ded3d19cafba49cbb0cd9
Maxim Soloviev <glarmaks@yandex.ru>
2016-09-27 00:33:11 +0300

    add proj

--- src/MrHey.java
+++ src/MrHey.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by GLARM on 06.09.2016.
  */
 public class MrHey {
-public static void main(String [] args){
-System.out.println("Hey Mr. 3");
-}
+
+    public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);
+        System.out.println("Input a, b: ");
+        int a = ss.nextInt();
+        int b = ss.nextInt();
+        mymethod(a, b);
+    }
+    public static void mymethod(int a, int b) {
+        if (a - b == 2)
+            System.out.println("a > b na 2");
+        else System.out.println("Неправильное значение");
+
+    }
 
 }
